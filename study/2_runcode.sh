697804b32c64f3d69a154f2dd67b3726e93ebb16
jiyu lee <30502827+bobae080328@users.noreply.github.com>
2024-12-03 21:58:51 +0900

    Update and rename 2_runcode.txt to 2_runcode.sh

--- study/2_runcode.sh
+++ study/2_runcode.sh
@@ -1,6 +1,6 @@
 # PRScs 코드실행 (염색체 하나에 대한 코드, 진행상황 확인을 위해 일일이 하나씩 실행했으나, os 함수 사용하여 자동화도 가능)
 
-!python /content/PRScs/PRScs.py \
+python /content/PRScs/PRScs.py \
 --ref_dir=/content/drive/MyDrive/ldblk_ukbb_amr \
 --bim_prefix=/content/hu794D40_xym \
 --sst_file=/content/output_gwas_adhd2.txt \
